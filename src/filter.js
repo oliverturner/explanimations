04adc07b70f842e44fc01cccc5101729bc8967b6
Oliver Turner <oliver.turner@codedsignal.co.uk>
2018-06-24 16:39:11 +0100

    Added filter

--- src/filter.js
+++ src/filter.js
@@ -6,12 +6,13 @@ const makeSource = numbers => {
   return numbers.map(n => `<li class="item"><span>${n}</span></li>`).join("");
 };
 
-const makeDestItem = n => {
+const makeDestItem = (n, x) => {
   const span = document.createElement("span");
   span.textContent = n;
 
   const li = document.createElement("li");
   li.className = "item item--dest";
+  li.style.cssText = `transform: translateX(${x}px) translateY(-90px)`;
   li.appendChild(span);
 
   return li;
@@ -22,29 +23,23 @@ const getXY = el => {
   return [x, y].map(Math.round);
 };
 
-const getResult = n => n * n;
+const getResult = n => n % 2 === 0;
 
-const updateFn = (numbers, msg) => numbers.forEach(el => (el.textContent = msg));
+const updateFn = (numbers, msg) =>
+  numbers.forEach(el => (el.textContent = msg));
 
-function Explanimation(total) {
+function Explanimation(els) {
+  const total = els.source.children.length;
   const numbers = makeNumbers(total);
-
-  const els = {
-    cb: document.querySelector("#callback"),
-    source: document.querySelector("#source"),
-    dest: document.querySelector("#dest"),
-    playBtn: document.querySelector("#play-btn"),
-    resetBtn: document.querySelector("#reset-btn"),
-    nums: document.querySelectorAll("#fn b")
-  };
 
   els.source.innerHTML = makeSource(numbers);
 
   // Work out the x coordinates of the transform props
+  const [sourceX] = getXY(els.source);
   const [callbackX] = getXY(els.cb);
-  const cbTransforms = [...els.source.children]
-    .map(getXY)
-    .map(xy => xy[0] - callbackX);
+  const xyz = [...els.source.children].map(getXY);
+  const positions = xyz.map(xy => xy[0] - sourceX);
+  const cbTransforms = xyz.map(xy => xy[0] - callbackX);
 
   // Set up TimelineLite
   //----------------------------------------------------------------------------
@@ -57,20 +52,24 @@ function Explanimation(total) {
   });
   cbTransforms.forEach((x, i) => {
     const n = numbers[i];
+    const p = positions[i];
     const t = 1;
     const v = getResult(n);
     const tween = {
       transform: `translateX(${x}px)`,
       onStart: () => updateFn(els.nums, n),
-      onComplete: () => els.dest.appendChild(makeDestItem(v))
+      onComplete: () => {
+        const { width } = els.dest.getBoundingClientRect();
+        v ? els.dest.appendChild(makeDestItem(n, p - width)) : null;
+      }
     };
     tl.to(els.cb, t, tween);
   });
 
   const reset = () => {
-    els.dest.classList.remove("is-active")
+    els.dest.classList.remove("is-active");
     els.dest.innerHTML = "";
-  }
+  };
 
   // Event handlers
   //----------------------------------------------------------------------------
@@ -80,10 +79,18 @@ function Explanimation(total) {
   });
 
   els.resetBtn.addEventListener("click", e => {
-    reset()
+    reset();
   });
 }
 
 (function() {
-  new Explanimation(7)
+  const els = {
+    cb: document.querySelector("#callback"),
+    source: document.querySelector("#source"),
+    dest: document.querySelector("#dest"),
+    playBtn: document.querySelector("#play-btn"),
+    resetBtn: document.querySelector("#reset-btn"),
+    nums: document.querySelectorAll("#fn b")
+  };
+  new Explanimation(els);
 })();
